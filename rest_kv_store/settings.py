7efd43cc14752c998914a661e918116935757546
Rabiul Islam <rabiulislam993@live.com>
2019-12-29 23:00:22 +0600

    added celery 4.2

--- rest_kv_store/settings.py
+++ rest_kv_store/settings.py
@@ -133,11 +133,11 @@ DEFAULT_TTL = 60 * 5  # in seconds
 
 # Celery application definition
 # http://docs.celeryproject.org/en/v4.0.2/userguide/configuration.html
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Asia/Dhaka'
 CELERY_BEAT_SCHEDULE = {
     'remove_expired_keys': {
@@ -145,3 +145,4 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': crontab()  # per minutes
     }
 }
+
